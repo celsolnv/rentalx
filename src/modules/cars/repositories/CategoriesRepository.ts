66646c30a0ef76da176dcc8d0301851bbd2bd8a1
Celso <celsolnv27@gmail.com>
2022-05-19 18:20:03 -0300

    DESING PATTERN: Aplicando o singleton na classe repository

--- src/modules/cars/repositories/CategoriesRepository.ts
+++ src/modules/cars/repositories/CategoriesRepository.ts
@@ -5,13 +5,22 @@ interface ICreateCategoryDTO {
   name: string;
   description: string;
 }
+
 class CategoryRepository implements ICategoriesRepository {
   private categories: Category[];
 
-  constructor() {
+  // eslint-disable-next-line no-use-before-define
+  private static INSTANCE: CategoryRepository;
+  private constructor() {
     this.categories = [];
   }
 
+  public static getInstance(): CategoryRepository {
+    if (!CategoryRepository.INSTANCE) {
+      CategoryRepository.INSTANCE = new CategoryRepository();
+    }
+    return CategoryRepository.INSTANCE;
+  }
   create({ description, name }: ICreateCategoryDTO) {
     const category = new Category();
 
